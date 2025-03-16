de72ba3bf7f9c491fd93605950df034b6f2dca6f
OrbyDoll <antonkhazin2006@gmail.com>
2025-03-16 22:14:12 +0300

    attempt 3 fix tests

--- src/test/java/repository/RecordsRepositoryTest.java
+++ src/test/java/repository/RecordsRepositoryTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.cassandra.CassandraInvalidQueryException;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.CassandraContainer;
@@ -25,6 +26,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest(classes = {Application.class, ApplicationConfig.class})
 @Testcontainers
+@ActiveProfiles("application-test")
 public class RecordsRepositoryTest {
   @Container
   private static final CassandraContainer<?> cassandraContainer =
